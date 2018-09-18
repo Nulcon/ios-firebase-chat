990bc13c0d876d3c3f4e30f7f98474b51c6e9799
Conner Alegre <cga0@pm.me>
2018-09-18 13:49:15 -0600

    initial commit

---
+++ ios-firebase-chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-firebase-chat
+//
+//  Created by Conner on 9/18/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
