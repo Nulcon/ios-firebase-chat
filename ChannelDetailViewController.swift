07ca0f40513e2cafdd7d7a17c52f79b131e773bd
Conner Alegre <cga0@pm.me>
2018-09-18 14:42:45 -0600

    add detail view for channel messages with ct base

---
+++ ChannelDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ChannelDetailViewController.swift
+//  ios-firebase-chat
+//
+//  Created by Conner on 9/18/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import UIKit
+// TODO: Add MessageKit
+
+class ChannelDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
